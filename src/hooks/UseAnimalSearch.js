c9fabe8d30a9b125d0b98a69514d875151e51169
Allan Regush <allan@allanregush.com>
2024-06-01 10:32:30 -0600

    chore: add prettier

--- src/hooks/UseAnimalSearch.js
+++ src/hooks/UseAnimalSearch.js
@@ -1,26 +1,25 @@
 import { useState, useEffect } from "react";
 
 export function useAnimalSearch() {
-    const [animals, setAnimals] = useState([]);
+  const [animals, setAnimals] = useState([]);
 
-    async function search(q) {
-      try {
-        const result = await fetch(
-          'http://localhost:8080?' + new URLSearchParams({ q })
-        );
-        const data = await result.json();
-        setAnimals(data);
-        localStorage.setItem('lastQuery', q);
-      }
-      catch (e) {
-        console.error(e);
-      }
+  async function search(q) {
+    try {
+      const result = await fetch(
+        "http://localhost:8080?" + new URLSearchParams({ q }),
+      );
+      const data = await result.json();
+      setAnimals(data);
+      localStorage.setItem("lastQuery", q);
+    } catch (e) {
+      console.error(e);
     }
-  
-    useEffect(() => {
-      const lastQuery = localStorage.getItem('lastQuery');
-      search(lastQuery);
-    }, [])
+  }
 
-    return { search, animals };
+  useEffect(() => {
+    const lastQuery = localStorage.getItem("lastQuery");
+    search(lastQuery);
+  }, []);
+
+  return { search, animals };
 }
